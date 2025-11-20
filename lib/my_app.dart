8ef91a2c59d4b74cfad277976ecdbcfb58d60c4a
3592917 <3592917@alu.murciaeduca.es>
2025-11-20 18:35:39 +0100

    Separado main.dart

---
+++ lib/my_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:pmdm_2526_1c_1e_r1/pages/my_home/my_home_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
